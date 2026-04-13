fef5e10faa9c61854c9ac0047f6bdc99199c6867
ThomasRuquet <thomas.ruquet@outlook.com>
2026-04-13 13:36:38 -0400

    Ajout des fichiers de base

---
+++ ETU/app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use OpenApi\Attributes as OA;
+
+define("OK", 200);
+define("CREATED", 201);
+define("NO_CONTENT", 204);
+define('UNAUTHORIZED', 401);
+define("NOT_FOUND", 404);
+define("INVALID_DATA", 422);
+define("SERVER_ERROR", 500);
+
+define("SONGS_PAGINATION", 10);
+
+#[OA\Info(title: "API Auth", version: "1.0")]
+#[OA\Server(url: "/", description: "Serveur local")]
+//https://swagger.io/docs/specification/v3_0/authentication/bearer-authentication/
+#[OA\SecurityScheme(
+    securityScheme: "bearerAuth",
+    type: "http",
+    scheme: "bearer",
+    bearerFormat: "Token Sanctum",
+    description: "Utiliser Authorization: Bearer {token}"
+)]
+
+abstract class Controller
+{
+    //
+}
---
+++ ETU/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\ReviewController;
+
+Route::middleware('throttle:5,1')->group(function () {
+    Route::post('/signup', 'App\Http\Controllers\AuthController@register');
+    Route::post('/signin', 'App\Http\Controllers\AuthController@login');
+});
+
+Route::middleware(['auth:sanctum', 'throttle:5,1'])->group(function () {
+    Route::get('/me', 'App\Http\Controllers\AuthController@me');
+    Route::post('/refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('/signout', 'App\Http\Controllers\AuthController@logout');
+});
