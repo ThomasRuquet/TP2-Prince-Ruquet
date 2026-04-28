4f79da8c185f9dba8950a24350995b85500147cf
Xavier Claessens <nymalevrai@gmail.com>
2026-04-27 22:46:18 -0400

    commit de la documentation

--- ETU/app/Http/Controllers/UserRentalController.php
+++ ETU/app/Http/Controllers/UserRentalController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Repository\RentalRepositoryInterface;
+use OpenApi\Attributes as OA;
 
 class UserRentalController extends Controller
 {
@@ -17,6 +18,23 @@ class UserRentalController extends Controller
     /**
      * Display a listing of the resource.
      */
+    #[OA\Get(
+        path: "/api/locations",
+        summary: "shows all active locations of user (throddling de 5 par minute)",
+        tags: ["UserRentalController"],
+        security: [["bearerAuth" => []]],
+        responses: [
+            new OA\Response(
+                response: 200, description: "les locations ont été retournées avec succès"
+            ),
+            new OA\Response(
+                response: 401, description: "l'utilisateur n'est pas connecté"
+            ),
+            new OA\Response(
+                response: 422, description: "Données invalides"
+            )
+        ]
+    )]
     public function getActiveRentals()
     {
         try{
